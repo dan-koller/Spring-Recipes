e021b4e618f848a44e6297f7bdfb7643726c6084
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-02-02 16:15:42 +0100

    Added basic project files

---
+++ src/main/java/io/github/dankoller/springrecipe/SpringrecipeApplication.java
@@ -0,0 +1,13 @@
+package io.github.dankoller.springrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrecipeApplication.class, args);
+	}
+
+}
